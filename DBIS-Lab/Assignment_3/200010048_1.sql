cdfbad7acfa17d6436c895d0dcf27b89a24a5c66
Shashank P <shashankp2832@gmail.com>
2022-09-04 01:29:50 +0530

    Added 5 i and 5 ii

--- DBIS-Lab/Assignment_3/200010048_1.sql
+++ DBIS-Lab/Assignment_3/200010048_1.sql
@@ -41,3 +41,29 @@ insert into supplier values(10, 'John', 'Paris', 'Citi-Bank');
 insert into shipment values(20, 15, 10, '2022-09-03', 50, 10);
 
 /* Question 5 */
+
+/* Part (i) */
+select distinct supplier.supplier_no, supplier.sup_name
+from (shipment natural join part) natural join supplier
+where part.color='red';
+
+/* Part (ii) */
+select supplier.supplier_no, supplier.sup_name, supplier_costs.total_cost, supplier.bank
+from supplier natural join (
+        select shipment.supplier_no, sum(shipment.quantity * shipment.price)
+        from shipment natural join supplier
+        group by shipment.supplier_no
+    ) 	as supplier_costs(supplier_no, total_cost);
+
+/* Part (iii) */
+select supplier_no, sup_name
+from supplier as S
+where not exists (
+    select part_no
+    from part
+    
+    except
+
+    select T.part_no
+    from supplier natural join shipment as T
+);
