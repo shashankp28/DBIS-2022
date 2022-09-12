4ec3573d27b7b8b55c13d9050d5fcde729356f1d
Shashank P <shashankp2832@gmail.com>
2022-09-12 11:58:02 +0530

    Assignemnt 4 done

---
+++ DBIS-Lab/Assignment_4/200010048.sql
@@ -0,0 +1,28 @@
+select distinct(course.course_id), course.title, instructor.ID, instructor.name
+from (instructor natural join teaches), course
+where course.course_id=teaches.course_id 
+	and instructor.dept_name='Comp. Sci.'
+	and teaches.year=2009 
+    and course.dept_name='Comp. Sci.'
+order by instructor.name ASC;
+
+
+insert into course values('CS-303', 'DBIS', 'Comp. Sci.', 6),
+                        ('CS-333', 'New Course', 'Comp. Sci.', 3);
+insert into prereq values('CS-333', 'CS-303');
+select * from prereq;
+
+update instructor
+set salary = salary*1.1
+where dept_name in (select department.dept_name 
+                    from department 
+                    where departmenet.budget>900000);
+
+
+select count(ID), course_id
+from student natural join takes
+where takes.course_id like "CS%"
+    and takes.year=2009
+    and takes.semester="Fall"
+group by course_id
+having count(ID)>2; 
