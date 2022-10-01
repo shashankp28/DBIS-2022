8ba6c2d9603950a01c2163efa2dc8d55c26b9c12
shashankp28 <shashankp2832@gmail.com>
2022-10-01 18:22:59 +0530

    Renamed FIles

--- DBIS-Lab/Assignment_5/AdvisorServlet.java
+++ DBIS-Lab/Assignment_5/AdvisorServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class StudentServlet extends HttpServlet{
+public class AdvisorServlet extends HttpServlet{
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
